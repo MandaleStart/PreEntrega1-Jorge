d63df4d3e9e0ee9ceaa9a3445b0b2115b2b99771
Nicolasjorge <MandaleStart@gmail.com>
2023-06-03 15:38:26 -0300

    v0.1.1

--- lucrecia-pharmacy-app/src/components/Main/LoadZone/LoadZone.jsx
+++ lucrecia-pharmacy-app/src/components/Main/LoadZone/LoadZone.jsx
@@ -1,37 +1,33 @@
 import { useEffect, useState } from 'react';
 import ItemListContainer from './ItemListContainer/ItemListContainer';
+import Card from './Card/Card';
 
 const URL_PROD = "http://127.0.0.1:3000/productos/";
 
-const ApiFetch = async () => {
-    try {
-        const response = await fetch(URL_PROD, {
+const LoadZone = () => {
+    const [productos, setProductos] = useState([]);
+
+    useEffect(() => {
+        fetch(URL_PROD, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
             }
-        });
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.log('Error:', error);
-        return [];
-    }
-};
-
-const LoadZone = () => {
-    const [productos, setProductos] = useState([]);
-    console.log(productos)
-    useEffect(() => {
-        ApiFetch().then(data => {
-            setProductos(data);
-        });
+        })
+        .then((response) => response.json())
+        .then((data) => setProductos(data));
     }, []);
 
     return (
         <div className="col-8 col-order-2">
-            <ItemListContainer greeting={"este es el ItemListContainer con un mensaje en greeting"} />
-            {productos}
+            <ItemListContainer greeting="este es el ItemListContainer con un mensaje en greeting" />
+            <div className="Cards-List">
+                {productos.map((producto) => (
+                    <div key={producto.id}>
+                        <Card data={producto} />
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
